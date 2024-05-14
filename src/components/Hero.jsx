652efda059e4009b9b1a07177adceb16db9a1684
krypton-0x00 <shakirgulzar0x00@gmail.com>
2024-05-14 13:33:28 +0100

    cleanup

---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { name, role } from "../config/config";
+import { dummyimg } from "../assets/assets";
+import { FaDove } from "react-icons/fa";
+
+const Hero = () => {
+  return <div></div>;
+};
+
+export default Hero;
